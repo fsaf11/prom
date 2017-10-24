9cdf8a419fd76597c1263e3d1281a39cdd71691d
Fernando dos Santos Alves Fernandes (Dino) <fsaf11@yahoo.com.br>
2017-10-23 23:13:28 -0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-sudo su & cd /usr/local & mkdir prometheus_components & cd /usr/local/prometheus_components & mkdir python_scripts & cd /usr/local/prometheus_components/python_scripts & yum -y install python-pip & pip install --upgrade pip & pip2 install prometheus-client
+sudo su && cd /usr/local && sudo mkdir prometheus_components && cd /usr/local/prometheus_components && sudo mkdir python_scripts && cd /usr/local/prometheus_components/python_scripts && yum -y install python-pip && pip install --upgrade pip && pip2 install prometheus-client
 wget https://github.com/fsaf11/prom/blob/master/ibm_machine_monitor.py &
 #create machines.txt
 python2 ibm_machine_monitor.py &
