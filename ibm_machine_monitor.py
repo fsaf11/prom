bfd28991e3147bda7c1760caeab869e18d1e04ff
Fernando dos Santos Alves Fernandes (Dino) <fsaf11@yahoo.com.br>
2017-10-26 09:07:45 -0200

    Update ibm_machine_monitor.py

--- ibm_machine_monitor.py
+++ ibm_machine_monitor.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 from os import path
 
 import subprocess
@@ -13,7 +13,7 @@ LOC_HOSTNAME = platform.node()
 
 # --- Metrics:
 statusPingTo = Gauge('ibm_status_ping', 'Status host (by ping)!', [ 'ip', 'orig_hostname', 'dest_hostname' ])
-#status_traceroute = Gauge('status_traceroute', 'Status host (by traceroute)!);
+statusTracerouteTo = Gauge( 'ibm_status_traceroute', 'Status host (by traceroute)!', [ 'ip', 'orig_hostname', 'dest_hostname' ]) ;
 
 PATH='./machines.txt'
 
@@ -23,13 +23,25 @@ def get_metrics():
     for line in machines:
         MACHINE = line.replace("\n", "")
         IP, HOSTNAME = MACHINE.split(" ")
+        
+        # Setting ping metric:
         command = 'ping -c 5 %s | tail -n 2 | grep "0 received"' % IP
         ping = subprocess.call(command, shell=True)
         if ping == 1:
             statusPingTo.labels( ip=IP, orig_hostname=LOC_HOSTNAME, dest_hostname=HOSTNAME ).set( 1 )
         else:
             statusPingTo.labels( ip=IP, orig_hostname=LOC_HOSTNAME, dest_hostname=HOSTNAME ).set( 0 )
-
+        
+        # Setting traceroute metric:
+        command = 'result=`traceroute -n %s | tail -n 1 | grep %s | cut -d " " -f3` && if [ "$result" = "%s" ] ; then echo $result; echo 1 ; else echo $result; echo -1; fi' % (IP,IP,IP)
+        traceroute = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+        out = traceroute.communicate()[0]
+        a, result, b = out.split( "\n" )
+        if int( result ) == 1:
+            statusTracerouteTo.labels( ip=IP, orig_hostname=LOC_HOSTNAME, dest_hostname=HOSTNAME ).set( 1 )
+        else:
+            statusTracerouteTo.labels( ip=IP, orig_hostname=LOC_HOSTNAME, dest_hostname=HOSTNAME ).set( 0 )   
+        
 def main():
     start_http_server(8001)
     print("Running on 8001")
